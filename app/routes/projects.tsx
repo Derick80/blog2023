25c7ed14ce8e574837eee7ca9664abcc4216a6be
Derick80 <derickchoskinson@gmail.com>
2023-04-19 23:06:05 -0500

    projects and api token

---
+++ app/routes/projects.tsx
@@ -0,0 +1,10 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { isAuthenticated } from "~/server/auth/auth.server";
+import { json, redirect } from "@remix-run/node";
+export async function loader({ request, params }: LoaderArgs) {
+  const user = await isAuthenticated(request);
+  if (!user) {
+    return redirect("/login");
+  }
+  return json({ user });
+}
