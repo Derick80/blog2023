04b3e8a841a73129d2f15c6e56aced9587d13fb0
Derick80 <derickchoskinson@gmail.com>
2023-05-19 23:19:36 -0500

    update

--- app/routes/blog_.new.tsx
+++ app/routes/blog_.new.tsx
@@ -210,7 +210,7 @@ export default function NewPostRoute() {
                     className="w-[42px] h-[25px] bg-blackA9 rounded-full relative shadow-[0_2px_10px] shadow-blackA7 focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default"
                     id='featured'
                     name='featured'
-                    style={{ '-webkit-tap-highlight-color': 'rgba(0, 0, 0, 0)' }}
+                    style={{ WebkitTapHighlightColor: 'transparent' }}
 
                     >
                              <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA7 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
