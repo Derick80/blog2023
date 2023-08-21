6ab85422f02e4c98d43a0aa1d706497646281786
Derick <derickchoskinson@gmail.com>
2023-08-21 14:29:53 -0500

    upldate layout

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className='flex h-auto w-full flex-col font-robo sm:p-1 md:flex-row  md:p-2 lg:flex-col lg:p-3'>
       <NavigationBar />
-      <div className='flex h-full w-full max-w-screen-lg flex-col p-2 md:p-4'>
+      <div className='mx-auto flex h-full w-full max-w-screen-lg flex-col p-2 md:p-4'>
         {children}
       </div>
       <ScrollToTop />
