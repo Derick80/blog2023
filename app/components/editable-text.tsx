29b7b5bf130a005b33527d4ac008bd13d3455070
Derick <derickchoskinson@gmail.com>
2024-02-16 20:05:44 -0600

    maybe final update

--- app/components/editable-text.tsx
+++ app/components/editable-text.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Input } from './ui/input'
 import { CornerDownLeft, CornerLeftDownIcon, Pencil } from 'lucide-react'
+import { Button } from './ui/button'
 
 type EditableTextFieldProps = {
   initialValue?: string
@@ -37,29 +38,35 @@ const EditableTextField = ({ initialValue }: EditableTextFieldProps) => {
             autoFocus
             className='border-b border-gray-500 focus:border-blue-500 w-full pr-10'
           />
-          <button
-            type='button'
+          <Button
+                      type='button'
+                      variant='ghost'
+                      onClick={() => setIsEditing(false)}
             className='absolute inset-y-0 right-0 px-2 py-1 text-blue-500 hover:text-blue-700 focus:outline-none'
           >
                       <CornerDownLeft
             size='14'
-
-                      /> {/* Icon for saving */ }
-          </button>
+        />
+          </Button>
         </>
       ) : (
         <>
                       <Input onClick={ handleDoubleClick } className='flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-muted-foreground pr-10 outline-none ring-1 ring-ring border-b cursor-text' value={
                           value ? value : 'Click to edit'
-                        } />
+                        }
+                        onChange={handleInputChange}
+
+                      />
 
 
-          <button
-            type='button'
-            className='absolute inset-y-0 right-0 px-2 py-1 text-blue-500 hover:text-blue-700 focus:outline-none'
+          <Button
+                          type='button'
+                          variant='ghost'
+                            onClick={handleDoubleClick}
+            className='absolute inset-y-0 right-0 px-2 py-1 text-blue-500 hover:text-blue-700 outline-none'
           >
             <Pencil size='14' />
-          </button>
+          </Button>
         </>
       )}
     </div>
