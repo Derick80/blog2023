798cc75b118f905b206f67c460750e27c391cdef
Derick <derickchoskinson@gmail.com>
2024-03-29 10:42:42 -0500

    markdown imp

--- app/routes/beta.tsx
+++ app/routes/beta.tsx
@@ -1,28 +1,67 @@
-import { type LoaderFunctionArgs, json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { ActionFunctionArgs, type LoaderFunctionArgs, json, redirect } from '@remix-run/node'
+import { ShouldRevalidateFunction, useLoaderData } from '@remix-run/react'
+import MarkdownEditorDiy from '~/components/beta/markdown-editor-diy'
 import ImageController from '~/components/images/image-controller'
 import { isAuthenticated } from '~/server/auth/auth.server'
 import { getImages } from '~/server/cloudinary.server'
 import { prisma } from '~/server/prisma.server'
+import { processMarkdownToHtml } from '~/server/markdown.server'
+export const shouldRevalidate: ShouldRevalidateFunction = ({
+  currentUrl,
+  nextUrl,
+}) => {
+  if (currentUrl.pathname === nextUrl.pathname) {
+    return false
+  }
+  return true
+}
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const user = await isAuthenticated(request)
   if (!user) {
-    throw new Error('Not authenticated')
+    return redirect('/login')
+
   }
+const content = 'Hello from the server!'
 
 
+  return json({ user, content })
+}
 
-  return json({ user,})
+export async function action ({
+  params,
+  request,
+}: ActionFunctionArgs) {
+  const formData = await request.formData()
+  const content = formData.get("content")?.toString() || ""
+console.log(content,'content server side');
+
+  const html = await processMarkdownToHtml(content)
+  console.log(html,'html server side');
+
+  // Optionally, store this in a database
+  // const id = params.id as string
+  // db[id].content = content.toString()
+  // db[id].preview = html.content
+  return new Response(html, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+  })
 }
 
 export default function Beta () {
+  const { user, content } = useLoaderData<typeof loader>()
 
-  const { user } = useLoaderData<typeof loader>()
 
 
   return (
     <div className='flex flex-col items-center  w-full h-full space-y-4'>
       <h1>Post</h1>
+
+      <MarkdownEditorDiy
+        content={ content }
+      />
     </div>
   )
 }
--- remix.config.ts
+++ remix.config.ts
@@ -1,14 +1,32 @@
+import { promise, string } from 'zod';
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   serverModuleFormat: "esm",
-serverDependenciesToBundle: [/^remix-utils.*/,/^remix-auth-totp.*/],
+  serverDependenciesToBundle: [/^remix-utils.*/, /^remix-auth-totp.*/,],
+  browserNodeBuiltinsPolyfill: {
+    modules: {
+      module: true,
+      util: true,
+      path: true,
+      crypto: true,
+      asserts: true,
+      stream: true,
+      fs: true,
+      promise: true,
+      string_decoder: true,
+      os: true,
+      child_process: true,
+      buffer: true,
+      tty: true,
+      worker_threads: true,
+  }
+}
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  future: {
 
-  },
 };
